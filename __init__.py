9e9f663b22ca89dd064f5b4ba4df97647e4a0d09
anderson <you@example.com>
2022-10-10 21:30:03 -0300

    add print image

--- __init__.py
+++ __init__.py
@@ -1,29 +1,45 @@
 import tkinter as tk
 from tkinter import filedialog
+from PIL import ImageTk
+import cv2
 
-ROOT_SCREEN_WIDTH = 300
-ROOT_SCREEN_HEIGHT = 200
+ROOT_SCREEN_WIDTH = 400
+ROOT_SCREEN_HEIGHT = 500
 
-def onOpen():
-    print(filedialog.askopenfilename(initialdir = "/",title = "Open file",filetypes = (("Img files",".png"),("Img files",".jpg"))))
+def onOpenImage():
+    imagePath = filedialog.askopenfilename(initialdir = "/",title = "Open file",filetypes = (("Img files",".png"),("Img files",".jpg")))
+    printImage(imagePath)
 
-def onSave():
-    print(filedialog.asksaveasfilename(initialdir = "/",title = "Save as",filetypes = (("Img files",".png"),("Img files",".jpg"))))
+def printImage(imagePath):
+    if(len(imagePath) > 0):
+        image = ImageTk.PhotoImage(file=imagePath)
+        imagebox.config(image=image)
+        imagebox.image = image
+
+        image = cv2.imread(imagePath)
+        oriImage = image.copy()
+
+def onSaveImage():
+    filename_save = filedialog.asksaveasfilename(initialdir = "/",title = "Save as",filetypes = (("Img files",".png"),("Img files",".jpg")))
 
 
 root_window = tk.Tk()
 
 root_window.geometry("{}x{}".format(ROOT_SCREEN_HEIGHT,ROOT_SCREEN_WIDTH))
-root_window.title("Image")
+root_window.title("Diagnosis of femorotibial osteoarthritis")
 
 menubar = tk.Menu(root_window)
 
 filemenu = tk.Menu(menubar, tearoff = 0)
-filemenu.add_command(label = "Open Image", command=onOpen)
-filemenu.add_command(label = "Save", command=onSave)
+filemenu.add_command(label = "Open Image", command=onOpenImage)
+filemenu.add_command(label = "Save", command=onSaveImage)
 filemenu.add_command(label = "Exit")
 
 menubar.add_cascade(label = "File", menu=filemenu)
 
 root_window.config(menu=menubar)
+
+imagebox = tk.Label(root_window)
+imagebox.pack()
+
 root_window.mainloop()
