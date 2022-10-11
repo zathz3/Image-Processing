240b569bef556c627c195b53c3533d342ea95382
anderson <you@example.com>
2022-10-11 14:54:57 -0300

    add main func

--- __init__.py
+++ __init__.py
@@ -6,10 +6,12 @@ import cv2
 ROOT_SCREEN_WIDTH = 400
 ROOT_SCREEN_HEIGHT = 500
 
+# função executada ao selecionar a opção "File > Open Image"
 def onOpenImage():
     imagePath = filedialog.askopenfilename(initialdir = "/",title = "Open file",filetypes = (("Img files",".png"),("Img files",".jpg")))
     printImage(imagePath)
 
+# função responsável pela exibição da imagem carregada na janela do programa.
 def printImage(imagePath):
     if(len(imagePath) > 0):
         image = ImageTk.PhotoImage(file=imagePath)
@@ -22,24 +24,28 @@ def printImage(imagePath):
 def onSaveImage():
     filename_save = filedialog.asksaveasfilename(initialdir = "/",title = "Save as",filetypes = (("Img files",".png"),("Img files",".jpg")))
 
+def main():
+    root_window = tk.Tk()
 
-root_window = tk.Tk()
+    root_window.geometry("{}x{}".format(ROOT_SCREEN_HEIGHT,ROOT_SCREEN_WIDTH))
+    root_window.title("Diagnosis of femorotibial osteoarthritis")
 
-root_window.geometry("{}x{}".format(ROOT_SCREEN_HEIGHT,ROOT_SCREEN_WIDTH))
-root_window.title("Diagnosis of femorotibial osteoarthritis")
+    menubar = tk.Menu(root_window)
 
-menubar = tk.Menu(root_window)
+    # Opções do menu superior
+    filemenu = tk.Menu(menubar, tearoff = 0)
+    filemenu.add_command(label = "Open Image", command=onOpenImage)
+    filemenu.add_command(label = "Save", command=onSaveImage)
+    filemenu.add_command(label = "Exit")
 
-filemenu = tk.Menu(menubar, tearoff = 0)
-filemenu.add_command(label = "Open Image", command=onOpenImage)
-filemenu.add_command(label = "Save", command=onSaveImage)
-filemenu.add_command(label = "Exit")
+    menubar.add_cascade(label = "File", menu=filemenu)
 
-menubar.add_cascade(label = "File", menu=filemenu)
+    root_window.config(menu=menubar)
 
-root_window.config(menu=menubar)
+    imagebox = tk.Label(root_window)
+    imagebox.pack()
 
-imagebox = tk.Label(root_window)
-imagebox.pack()
+    root_window.mainloop()
 
-root_window.mainloop()
+if __name__ == "__main__":
+    main()
