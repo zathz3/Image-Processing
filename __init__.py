3a0e2519a0859cff77aa6c90958037856491ab5e
Joao Gabriel <joao2200@hotmail.com>
2022-10-11 23:25:12 -0300

    part3

--- __init__.py
+++ __init__.py
@@ -2,22 +2,41 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import ImageTk
 import cv2
+import os
 
 ROOT_WINDOW_HEIGHT = 500
 ROOT_WINDOW_WIDTH = 400
 
+img_name = ""
+image_path = ""
+
 IS_CROPPED = False
 CROPPING_EVENT_FINISHED = False
 LOADED_IMG = None
+CROP_IMG = None
+
+x_start, y_start, x_end, y_end = 0, 0, 0, 0
+cropping = False
+image = None
+oriImage = None
 
 # função executada ao selecionar a opção "File > Open Image"
 def onOpenImage():
+    global imagePath
     imagePath = filedialog.askopenfilename(initialdir = "./tests",title = "Open file",filetypes = (("Img files",".png"),("Img files",".jpg")))
+    global img_name
+    img_name = imagePath[-12::]
     printImage(imagePath)
+
+
+# def getCropDirectory(crop_name):
+#     dir_path = os.path.dirname(os.path.realpath(__file__))
+#     crop_path = dir_path + crop_name
 
 # função responsável pela exibição da imagem carregada na janela do programa.
 def printImage(imagePath):
     if(len(imagePath) > 0):
+        global oriImage, LOADED_IMG
         LOADED_IMG = ImageTk.PhotoImage(file=imagePath)
         imagebox.config(image=LOADED_IMG)
         imagebox.image = LOADED_IMG
@@ -26,15 +45,66 @@ def printImage(imagePath):
         oriImage = LOADED_IMG.copy()
         menubar.entryconfig("Crop", state="active")
 
-def cropImage():
-    IS_CROPPED = False
-    cv2.namedWindow("image")
-    cv2.setMouseCallback("image", mouseCrop)
 
-def mouseCrop():
+def printCrop(cropPath):
+    if (len(imagePath) > 0):
+        global LOADED_IMG, CROP_IMG
+
+
+def onOpenCrop():
+    cropPath = filedialog.askopenfilename(initialdir="./crops", title="Open file",
+                                           filetypes=(("Img files", ".png"), ("Img files", ".jpg")))
+    printCrop(cropPath)
+
+
+# Funcao para detectar os movimentos do mouse e realizar o crop
+def mouse_crop(event, x, y, flags, param):
+    global x_start, y_start, x_end, y_end, cropping, pathImagemRecortada, cropped, i
     if event == cv2.EVENT_LBUTTONDOWN:
+        # Ao pressionar o mouse, armazena posicao inicial e final
         x_start, y_start, x_end, y_end = x, y, x, y
-        CROPPING_EVENT_FINISHED = True
+        cropping = True
+    elif event == cv2.EVENT_MOUSEMOVE:
+        # movimentacao do mouse
+        if cropping == True:
+            x_end, y_end = x, y
+    elif event == cv2.EVENT_LBUTTONUP:
+        # soltar left click
+        x_end, y_end = x, y
+        # finaliza o crop
+        cropping = False
+        refPoint = [(x_start, y_start), (x_end, y_end)]
+        if len(refPoint) == 2:
+            roi = oriImage[refPoint[0][1]:refPoint[1][1], refPoint[0][0]:refPoint[1][0]]
+            crop_name = "crops/" + img_name
+            cv2.imwrite(crop_name,roi)
+            cropped = True
+
+
+def crop():
+    global cropped, img
+    cropped = False
+    cv2.namedWindow("image")
+
+    while not cropped:
+        try:
+            cv2.setMouseCallback("image", mouse_crop)
+        except:
+            print("Window Closed")
+            cropped = True
+
+        img = LOADED_IMG.copy()
+        if not cropping:
+            #mostra imagem na janela
+            cv2.imshow("image", LOADED_IMG)
+        if cropping:
+            #imagem, ponto inicial, ponto final, cor, largura
+            cv2.rectangle(img, (x_start, y_start), (x_end, y_end), (0, 255, 0), 2)
+            cv2.imshow("image", img)
+        cv2.waitKey(1)
+    cv2.destroyWindow("image")
+
+
 
 def onSaveImage():
     filename_save = filedialog.asksaveasfilename(initialdir = "./tests",title = "Save as",filetypes = (("Img files",".png"),("Img files",".jpg")))
@@ -52,11 +122,12 @@ menubar = tk.Menu(root)
 # Opções do menu superior
 filemenu = tk.Menu(menubar, tearoff = 0)
 filemenu.add_command(label = "Open image ...", command=onOpenImage)
+filemenu.add_command(label = "Open crop ...", command=onOpenCrop)
 filemenu.add_command(label = "Save", command=onSaveImage)
 filemenu.add_command(label = "Exit", command=closeWindow)
 
 menubar.add_cascade(label = "File", menu=filemenu)
-menubar.add_command(label = "Crop", state= "disabled", command=cropImage)
+menubar.add_command(label = "Crop", state= "disabled", command=crop)
 
 root.config(menu=menubar)
 
